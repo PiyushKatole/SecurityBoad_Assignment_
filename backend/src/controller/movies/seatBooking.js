3d2c56796ba179023886c1ce05a9d37987bc76be
Shivamkr025 <shivam22@navgurukul.org>
2024-05-31 19:57:48 +0530

    create movies controlle and defind all the operation

---
+++ backend/src/controller/movies/seatBooking.js
@@ -0,0 +1,69 @@
+const { Seat, Show, Booking } = require('../../model/moviesSchema');
+const {users} = require('../../model/userSchema')
+const {findConsecutiveSeats} = require('../../middleware/seatBookingMiddleware')
+
+const seatBooking = async (req, res) => {
+    const { user, movie, seats } = req.body;
+
+    try {
+        const findUser = await users.findOne({email:user});
+        if (!findUser) {
+            return res.status(400).json({ error: "Please create your account" });
+        }
+
+        const findShow = await Show.findOne({movie });
+        console.log(findShow);
+        if (!findShow) {
+            return res.status(400).json({ error: "Sorry, show is not found" });
+        }
+
+        const availableSeats = await Seat.find({ screen: findShow.screen, isBooked: false });
+
+        const bookedSeats = findConsecutiveSeats(availableSeats, seats);
+        if (!bookedSeats) {
+            return res.status(400).json({ error: `Not enough consecutive seats available` });
+        }
+
+        const totalPrice = bookedSeats.length * findShow.price;
+        const bookingTime = Date.now();
+
+        await Seat.updateMany({ _id: { $in: bookedSeats.map(seat => seat._id) } }, { $set: { isBooked: true } });
+
+        const booking = new Booking({
+            user: findUser.email,
+            show: findShow.movie,
+            seats: bookedSeats.map(seat => seat._id ),
+            bookingTime,
+            totalPrice
+        });
+
+        await booking.save();
+
+        res.status(201).json(booking);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Server error' });
+    }
+}
+
+const cancelSeatBooking = async (req, res) => {
+    const { user } = req.body;
+    try {
+        const booking = await Booking.findOne({ user: user });
+        if (!booking) {
+            return res.status(404).json({ error: "Booking not found" });
+        }
+
+        await Seat.updateMany({ _id: { $in: booking.seats } }, { $set: { isBooked: false } });
+
+        await Booking.deleteOne({ _id: booking._id });
+
+        res.status(200).json({ message: "Booking cancelled successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Server error' });
+    }
+}
+
+
+module.exports = {seatBooking , cancelSeatBooking};
